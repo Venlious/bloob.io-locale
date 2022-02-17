2a4c7ea88b6809df341d40c1e96688c580d3108f
Tristan Volk <git@tristanvolk.com>
2022-02-18 00:49:00 +0100

    Load languages on demand to reduce app size

---
+++ en/index.js
@@ -0,0 +1,4 @@
+import messages from './messages'
+import numberFormats from './numberFormats'
+
+export { messages, numberFormats }
--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-import msg_en from './en/messages'
-import msg_nl from './nl/messages'
-import msg_de from './de/messages'
-import number_en from './en/numberFormats'
-import number_nl from './nl/numberFormats'
-import number_de from './de/numberFormats'
-
-const messages = {
-	en: msg_en,
-	nl: msg_nl,
-	de: msg_de
-}
-
-const numberFormats = {
-	en: number_en,
-	nl: number_nl,
-	de: number_de
-}
-
-export { messages, numberFormats }
+const supportedLocales = ['en', 'nl', 'de']
+export { supportedLocales }
