a3766925921eb99ec09080c5daa8f9579ff981e5
Tristan Volk <git@tristanvolk.com>
2023-04-12 20:15:12 +0200

    Add verified files to keep track of which files have been checked by humans

--- helpers/fix/index.ts
+++ helpers/fix/index.ts
@@ -11,6 +11,7 @@ import * as osPath from 'path'
 // Data
 import { supportedLocales } from '../../index'
 const LOCALE_FOLDER = `../../messages`
+const EN_MESSAGES = require(`../../messages/en.json`)
 
 /**
  * Ensures the locale file is consistent with the source.
@@ -38,18 +39,82 @@ const checkAndFixLocale = async (source: string, target: string) => {
 		console.info(`Validated and fixed "${target}.json"...`)
 	} catch (error) {
 		console.error(error)
-		throw `[${target}] Failed to write output file.`
+		throw `Failed to validate or write output file for "${target}.json".`
+	}
+}
+
+const fixVerifyObject = (obj: any): void => {
+	if (Array.isArray(obj)) {
+		obj.forEach((item, index) => {
+			if (typeof item !== `boolean`) {
+				obj[index] = false
+			}
+		})
+	} else if (typeof obj === null) {
+		obj = false
+	} else if (typeof obj === `object`) {
+		for (const key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				const value = obj[key]
+				if (value !== null && typeof value === `object`) {
+					fixVerifyObject(value)
+				} else {
+					obj[key] = false
+				}
+			}
+		}
+	} else if (typeof obj !== `boolean`) {
+		obj = false
 	}
 }
 
 /**
- * Loops through all supported locales and fixes the following:
- * - Makes sure key order is consistent with source
- * - Adds missing entries present in source
- * - Removes entries that are not in source
- * - Makes sure the _meta.total and _meta.missing counts are updated
+ * Ensures the locale file is consistent with the source.
+ *
+ * @param target Target file to modify to match
  */
-supportedLocales.forEach(async locale => {
-	await checkAndFixLocale(`en`, locale)
-})
-checkAndFixLocale(`en`, `_empty`)
+const checkAndFixVerified = async (target: string) => {
+	const { LOCALE_SOURCE, LOCALE_TARGET } = await loadMessages(target, `verified/${target}`)
+
+	try {
+		// Add missing entries to the object
+		let data = addMissingEntriesToObject(LOCALE_SOURCE, LOCALE_TARGET)
+
+		// Ensure order is consistent with source
+		data = ensureSameOrder(EN_MESSAGES, data)
+
+		// Delete meta data
+		delete data._meta
+		fixVerifyObject(data)
+
+		// Write file
+		writeFile(osPath.join(__dirname, `${LOCALE_FOLDER}/verified/${target}.json`), data)
+		console.info(`Updated "verified/${target}.json"...`)
+	} catch (error) {
+		console.error(error)
+		throw `Failed to validate or write output file for "verified/${target}.json".`
+	}
+}
+
+const process = async () => {
+	/**
+	 * Loops through all supported locales and fixes the following:
+	 * - Makes sure key order is consistent with source
+	 * - Adds missing entries present in source
+	 * - Removes entries that are not in source
+	 * - Makes sure the _meta.total and _meta.missing counts are updated
+	 */
+	supportedLocales.forEach(async locale => {
+		await checkAndFixLocale(`en`, locale)
+	})
+	await checkAndFixLocale(`en`, `_empty`)
+
+	/**
+	 * Checks and validates the verified files for all languages.
+	 */
+	supportedLocales.forEach(async locale => {
+		await checkAndFixVerified(locale)
+	})
+}
+
+process()
