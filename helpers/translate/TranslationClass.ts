acf6d7d89833f6c017a84a2b41c32c415edd86b2
Tristan Volk <git@tristanvolk.com>
2026-06-02 20:40:36 +0200

    Add translations for opponent left decision in multiple languages

--- helpers/translate/TranslationClass.ts
+++ helpers/translate/TranslationClass.ts
@@ -337,6 +337,7 @@ export default class TranslationClass {
 		this.generateTranslationTask([`info`, `about`])
 		this.generateTranslationTask([`info`, `awaitingPurchaseConfirmation`])
 		this.generateTranslationTask([`info`, `readyUpWarning`])
+		this.generateTranslationTask([`info`, `opponentLeftDecision`])
 		this.generateTranslationTask(
 			[`info`, `recentlyPlayedWith`],
 			`These are used in the "Recently Played With" feature, which shows players you've recently played games with.`
